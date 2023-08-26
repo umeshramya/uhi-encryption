d96fda79d0c0b68d72ce8933117a3bec21fd0a24
unknown <umeshbilagi@gmail.com>
2023-08-26 08:19:25 +0530

    working with new uhi head genaeator

--- src/main/java/com/nicehms/learnspringboot/UhiControler.java
+++ src/main/java/com/nicehms/learnspringboot/UhiControler.java
@@ -1,51 +1,52 @@
 package com.nicehms.learnspringboot;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.util.Arrays;
-import java.util.List;
+import java.security.PublicKey;
+import java.util.Base64;
 import java.util.Map;
 
 import org.bouncycastle.jcajce.spec.EdDSAParameterSpec;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class UhiControler {
 
 	@RequestMapping("uhi/generateKeyspair")
-	public List<UhiKeys> getKeyPair() {
-		var crypto=new CryptUHI();
-		var keyPair = crypto.generateKeyPair(EdDSAParameterSpec.Ed25519, 256);
-		
-		return Arrays.asList(
-				new UhiKeys(crypto.getBase64Encoded(keyPair.getPrivate()), crypto.getBase64Encoded(keyPair.getPublic()))
-			);
-		
-		
+	public Map<String, String> getKeyPair() throws NoSuchAlgorithmException {
+		var crypto = new Crypt();
+		return crypto.generateDerKeyPairs();
+
 	}
-	
+
 	@RequestMapping("uhi/generateAuthorizationHeader")
-	public Map<String, String> generateAuthorizationParams
-	(String subscriberId, String pubKeyId, String payload, String privateKey) {
-		var cryto = new CryptUHI();
-		PrivateKey curKey = cryto.getPrivateKey(EdDSAParameterSpec.Ed25519, privateKey);		
+	public String generateAuthorizationParams(String subscriberId, String pubKeyId, String payload, String privateKey)
+			throws Exception {
+		var cryto = new Crypt();
+		PrivateKey curKey = cryto.getPrivateKey("Ed25519", Base64.getDecoder().decode(privateKey));
 		var header = cryto.generateAuthorizationParams(subscriberId, pubKeyId, payload, curKey);
 		return header;
-			
-		
+
 	}
-	
-	
+
 	@RequestMapping("uhi/verifysignature")
-	public boolean verifySignature(long created, long expires, String signature, String publicKey, String keyId, String data) {
+	public boolean verifySignature(String created, String expires, String signature, String publicKey, String keyId, String data) {
 		
 		try {
-			var crypt = new CryptUHI();
+			var crypt = new Crypt();
 //			Verify date
-			String hashedSigningString = crypt
-					.generateBlakeHash(crypt.getSigningString(Long.valueOf(created), Long.valueOf(expires), data));
-			boolean ret = crypt.verifySignature1(signature, hashedSigningString, publicKey);
+			// String hashedSigningString = crypt
+			// 		.generateBlakeHash(crypt.getSigningString(Long.valueOf(created), Long.valueOf(expires), data));
+			String hashedSigningString = crypt.generateBlakeHash(
+				crypt.getSigningString(Long.parseLong(created), Long.parseLong(expires), data));
+
+			PublicKey pub_key = Crypt.getPublicKey("Ed25519", Base64.getDecoder().decode(publicKey));
+			boolean ret = crypt.verifySignature(hashedSigningString, signature, "Ed25519",pub_key);
+
+					
+			// boolean ret = crypt.verifySignature(hashedSigningString, signature,EdDSAParameterSpec.Ed25519, publicKey );
+	
 			
 			return ret;
 			
